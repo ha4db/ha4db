a3c3145f5aa5e58347b3195840ef8b031a832662
Taro Matsuzawa <taro@georepublic.co.jp>
2020-08-24 11:06:58 +0900

    rename example to ha4db

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Example
+module Ha4db
   # Our Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
