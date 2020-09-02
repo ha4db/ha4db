abbb8078f1c3dd233d0e55b0c2184b5d4a323914
Taro Matsuzawa <taro@georepublic.co.jp>
2020-09-02 16:53:37 +0900

    WIP added MDC for web to admin/users#index

---
+++ app/javascript/packs/commons/button.ts
@@ -0,0 +1,6 @@
+import "../../src/commons/button.scss"
+import { MDCRipple } from '@material/ripple'
+
+new MDCRipple(document.querySelector('.mdc_button'))
+const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
---
+++ app/javascript/packs/commons/data-table.ts
@@ -0,0 +1,3 @@
+import '../../src/commons/data-table.scss'
+import {MDCDataTable} from '@material/data-table';
+new MDCDataTable(document.querySelector('.mdc-data-table'));
