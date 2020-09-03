1a2319ab3356a9e137b9fa8585bf28dcb144d5da
Taro Matsuzawa <taro@georepublic.co.jp>
2020-09-03 10:40:06 +0900

    WIP use fab to button

--- app/javascript/packs/commons/button.ts
+++ app/javascript/packs/commons/button.ts
@@ -4,3 +4,4 @@ import { MDCRipple } from '@material/ripple'
 new MDCRipple(document.querySelector('.mdc_button'))
 const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
 iconButtonRipple.unbounded = true;
+new MDCRipple(document.querySelector('.mdc-fab'))
