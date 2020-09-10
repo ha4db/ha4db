bed2b9e590ff1caa95499f5cc38594d02f260006
Taro Matsuzawa <taro@georepublic.co.jp>
2020-09-10 16:22:46 +0900

    WIP: added excel file upload function

--- app/controllers/mlit_importers_controller.rb
+++ app/controllers/mlit_importers_controller.rb
@@ -4,7 +4,16 @@
 class MlitImportersController < UserBaseController
   def new; end
 
-  def preview; end
+  def preview
+    update_file = preview_params[:upload_file]
+    @bridge = MlitImporter.import(update_file)
+  end
 
   def create; end
+
+  private
+
+  def preview_params
+    params.permit(:upload_file)
+  end
 end
--- spec/requests/mlit_importers_request_spec.rb
+++ spec/requests/mlit_importers_request_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe 'MlitImporters', type: :request do
   before do
     @user = FactoryBot.create(:user)
@@ -17,8 +18,12 @@ RSpec.describe 'MlitImporters', type: :request do
 
   describe 'GET /preview' do
     it 'returns http success' do
-      post preview_mlit_importer_url
+      file = File.join(File.dirname(__FILE__), '../testdata/mlit-testdata-001.xlsx')
+      post preview_mlit_importer_url, params: {
+        upload_file: Rack::Test::UploadedFile.new(file)
+      }
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('bridge[name]')
     end
   end
 
@@ -29,3 +34,4 @@ RSpec.describe 'MlitImporters', type: :request do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
