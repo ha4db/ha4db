32378774799bb95c01952db01e266835c232199d
Taro Matsuzawa <taro@georepublic.co.jp>
2020-10-13 07:02:45 +0900

    #22 seek allow nil

--- app/models/bridge_content_inspection.rb
+++ app/models/bridge_content_inspection.rb
@@ -6,7 +6,7 @@ class BridgeContentInspection < ApplicationRecord
   belongs_to :inspection
   store_accessor :data, :seek
   store_accessor :data, :position
-  validates :seek, numericality: { only_integer: true }
+  validates :seek, numericality: { only_integer: true }, allow_nil: true
 
   def create_inspection(bridge_content_inspection_params, inspection_params)
     new_inspection = Inspection.new(inspection_params.merge({ bridge: bridge_content.bridge }))
