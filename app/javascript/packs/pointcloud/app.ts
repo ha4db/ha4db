fa495a65b498b84e940d36e4028fe45975966d41
Itsuro Tajima <tajima@georepublic.de>
2021-02-07 14:10:45 +0000

    (refs #64) added annotation

--- app/javascript/packs/pointcloud/app.ts
+++ app/javascript/packs/pointcloud/app.ts
@@ -32,10 +32,7 @@ controls.panSpeed = 1.0;
 // initialize raycaster
 const raycaster = new THREE.Raycaster();
 document.addEventListener('click', raycast);
-const sphereGeometry = new THREE.SphereGeometry( 0.1, 32, 32 );
-const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-scene.add(sphere);
+let pastSprite = null;
 
 const tilesRenderer = new TilesRenderer( path );
 tilesRenderer.setCamera( camera );
@@ -63,16 +60,47 @@ function raycast(e) {
 		x: (e.offsetX / dom.clientWidth) * 2 - 1,
 		y: - (e.offsetY / dom.clientHeight) * 2 + 1
 	};
-	console.log(mouse);
 	raycaster.setFromCamera( mouse, camera );
 	const intersects = raycaster.intersectObject(tilesRenderer.group, true);
-	console.log(intersects);
 
 	if (intersects.length > 0) {
-		sphere.position.copy(intersects[0].point);
+		makeInspectionLabel(intersects[0].point)
 		return intersects[0];
 	}
 	else {
 		return false;
 	}
+}
+
+// make sprite for inspection
+function makeInspectionLabel(position) {
+	scene.remove(pastSprite);
+	// initialize
+	const canvasWidth = 50;
+	const canvasHeight = 50;
+	const fontSize = 40;
+	const text = '点検箇所';
+	// create canvas
+	const canvasForText = document.createElement('canvas');
+	const ctx = canvasForText.getContext('2d');
+	ctx.canvas.width = canvasWidth;
+	ctx.canvas.height = canvasHeight;
+	ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx.fillStyle = 'black';
+	ctx.font = `${fontSize}px`;
+	ctx.fillText(
+	  text,
+	  (canvasWidth - ctx.measureText(text).width) / 2,
+	  canvasHeight / 2 + ctx.measureText(text).actualBoundingBoxAscent / 2
+	);
+	// create texture
+	const texture = new THREE.CanvasTexture(canvasForText);
+	// create sprite
+	const spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+	const sprite = new THREE.Sprite(spriteMaterial);
+	sprite.scale.set(1, 1, 1);
+	sprite.position.set(position.x, position.y, position.z);
+	pastSprite = sprite;
+	scene.add(sprite);
 }
