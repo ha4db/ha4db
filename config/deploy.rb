52c1712b927889ed3fd96e36a139d041bc9a9677
Taro Matsuzawa <taro@georepublic.co.jp>
2021-02-18 13:59:28 +0900

    #76 set puma worker timeout

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,6 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# Puma settings
+set :puma_worker_timeout, 3600
