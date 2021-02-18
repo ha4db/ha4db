e6fa7434b0dd10db34d5351405bf0a8388e94fb3
Taro Matsuzawa <taro@georepublic.co.jp>
2021-02-18 18:22:34 +0900

    share storage, ortho and pointcloud

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,9 @@ set :branch, :main
 append :linked_files, 'config/credentials/production.key', 'config/credentials/production.yml.enc'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log',
+       'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system',
+       'storage', 'public/ortho_images', 'public/pointclouds'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
